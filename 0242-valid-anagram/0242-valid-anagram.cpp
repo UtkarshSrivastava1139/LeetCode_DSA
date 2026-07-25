17de82e4d193f38d03bffcb52eb496605692e237
Utkarsh Srivastava <utkarshsri1139@gmail.com>
2026-07-26 02:37:12 +0530

    Time: 8 ms (7.07%), Space: 10 MB (11.59%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> freq1;
+        unordered_map<char, int> freq2;
+
+        for(char c : s){
+            freq1[c]++;
+        }
+        for(char c : t){
+            freq2[c]++;
+        }
+
+        for(char c : t){
+            freq1.count(c);
+            freq2.count(c);
+            if(freq1 != freq2) return false;
+        }
+        return true;
+
+    }
+};
