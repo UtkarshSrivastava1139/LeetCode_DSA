9a68236b19a134d0bb6131e30f8796a0d50cf8fa
Utkarsh Srivastava <utkarshsri1139@gmail.com>
2026-07-26 02:22:02 +0530

    Time: 60 ms (48.5%), Space: 111.3 MB (23.27%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,9 +4,8 @@ public:
         unordered_set<int> seen;
 
         for(int i = 0; i<nums.size(); i++){
-            auto it = seen.find(nums[i]);
-            if(it != seen.end()) return true;
-            else seen.insert(nums[i]);
+            if(seen.find(nums[i]) != seen.end()) return true;
+            seen.insert(nums[i]);
         }
         return false;
     }
